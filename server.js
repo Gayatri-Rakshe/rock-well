7c20db88b9d45b0d30fe184790b004a6df31e0fa
Gayatri-Rakshe <98088347+Gayatri-Rakshe@users.noreply.github.com>
2022-01-29 11:54:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -12,12 +12,6 @@ app.use(express.static(path.join(__dirname,'public')));
 });
  
 
-app.get('/hello', function (req, res) {
-  console.log("CAlling rest api");
-  var person={firstName:'Ravi',lastName:'Tambade',age:43};
-  res.send(person);
-});
-
  
 var server = app.listen(8081, function () {
 
